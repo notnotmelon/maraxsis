47bf6c920f7447827912ac54bb5a2067ce1e7082
notnotmelon <zacharypicco@gmail.com>
2024-08-26 08:34:16 -0500

    accessibility settings

--- prototypes/tile/water.lua
+++ prototypes/tile/water.lua
@@ -12,10 +12,9 @@ local template = {
     flags = {'not-on-map', 'hidden'},
     collision_box = {{-16, -16}, {16, 16}},
     secondary_draw_order = -1,
-    collision_mask = {}
+    collision_mask = {},
 }
-
-for size = 0, 5 do
+for size = 0, 0 do
     size = 2 ^ size
     for i = 1, size do
         for j = 1, size do
@@ -29,9 +28,11 @@ for size = 0, 5 do
             water.icon_size = 32
             water.name = 'h2o-water-shader-' .. (32 / size) .. '-' .. j .. '-' .. i
             water.localised_name = water.name
+            local visiblity = tonumber(settings.startup['h2o-water-opacity'].value) / 255
             water.animations = {
                 layers = {
                     {
+                        tint = {r = visiblity, g = visiblity, b = visiblity, a = 1 / 255},
                         height = 256 / size,
                         width = 256 / size,
                         line_length = 32 * size,
@@ -46,7 +47,7 @@ for size = 0, 5 do
                     },
                 }
             }
-            data:extend{water}
+            data:extend {water}
         end
     end
 end
@@ -69,16 +70,16 @@ local waterifiy = {
         tile.draw_in_water_layer = true
         --tile.walking_sound = nil -- TODO: add a swimming sound
         tile.walking_speed_modifier = 0.2
-        water_tile_type_names[#water_tile_type_names+1] = tile.name
-    
+        water_tile_type_names[#water_tile_type_names + 1] = tile.name
+
         if not include_submarine_exclusion_zone then return {tile} end
-    
+
         local submarine_exclusion_zone = table.deepcopy(tile)
         submarine_exclusion_zone.layer = layer
         submarine_exclusion_zone.name = tile.name .. '-submarine-exclusion-zone'
         submarine_exclusion_zone.collision_mask = {maraxsis_collision_mask, 'rail-layer'}
-        water_tile_type_names[#water_tile_type_names+1] = submarine_exclusion_zone.name
-        
+        water_tile_type_names[#water_tile_type_names + 1] = submarine_exclusion_zone.name
+
         layer = layer + 1
         return {tile, submarine_exclusion_zone}
     end,
@@ -98,7 +99,7 @@ local waterifiy = {
         underwater = table.deepcopy(underwater)
         underwater.localised_name = underwater.localised_name or {'entity-name.' .. underwater.name}
         underwater.name = underwater.name .. '-underwater'
-        
+
         underwater.localised_name = underwater.localised_name or {'entity-name.' .. underwater.name}
         collision_mask_util.remove_layer(collision_mask_util.get_mask(underwater), maraxsis_collision_mask)
         collision_mask_util.add_layer(collision_mask_util.get_mask(underwater), 'ground-tile')
@@ -127,14 +128,14 @@ local function trenchifiy(tile)
 
         for _, picture in pairs(orientation.pictures) do
             local layer = table.deepcopy(picture.layers[1])
-            for _, version in pairs{layer, layer.hr_version} do
-                version.filename = version.filename:gsub('.png', '-'..tile..'.png')
+            for _, version in pairs {layer, layer.hr_version} do
+                version.filename = version.filename:gsub('.png', '-' .. tile .. '.png')
                 version.filename = version.filename:gsub('__base__/graphics/terrain/cliffs', '__dihydrogen-monoxide__/graphics/entity/cliffs')
             end
-            pictures[#pictures+1] = layer
+            pictures[#pictures + 1] = layer
         end
 
-        results[#results+1] = {
+        results[#results + 1] = {
             name = tile .. '-trench-' .. k:gsub('_', '-'),
             type = 'simple-entity',
             collision_box = {{-2, -2}, {2, 2}},
@@ -160,180 +161,9 @@ data:extend(trenchifiy('sand-1'))
 data:extend(trenchifiy('sand-3'))
 data:extend(trenchifiy('dirt-5'))
 
---[[local trench_wall_cliff = table.deepcopy(data.raw['cliff']['cliff'])
-trench_wall_cliff.name = 'cliff-trench-wall'
-trench_wall_cliff.localised_name = {'entity-name.trench-wall'}
-trench_wall_cliff.cliff_explosive = nil
-trench_wall_cliff.minable = nil
-trench_wall_cliff.selectable_in_game = false
-trench_wall_cliff.map_color = {0.2, 0.2, 0.2, 1}
-for k, orientation in pairs(trench_wall_cliff.orientations) do
-    for _, picture in pairs(orientation.pictures) do
-        for _, layer in pairs(picture.layers) do
-            layer.tint = {r = 0.2, g = 0.2, b = 0.3}
-            layer.hr_version.tint = {r = 0.2, g = 0.2, b = 0.3}
-        end
-    end
-end
-data:extend{trench_wall_cliff}--]]
-
--- NESW
-
-local X = 'sand'
-local O = 'water'
-local N = 'doesnt-matter'
-local corner_order = {
-    {{N, X, N},
-    {O, O, X},
-    {N, O, N}},
-    
-    {{N, O, N},
-    {O, O, X},
-    {N, X, N}},
-    
-    {{N, O, N},
-    {X, O, O},
-    {N, X, N}},
-    
-    {{N, X, N},
-    {X, O, O},
-    {N, O, N}},
-    
-    
-    {{O, O, X},
-    {O, O, O},
-    {N, O, N}},
-    
-    {{N, O, N},
-    {O, O, O},
-    {O, O, X}},
-    
-    {{O, O, O},
-    {O, O, O},
-    {X, O, O}},
-    
-    {{X, O, O},
-    {O, O, O},
-    {O, O, O}},
-
-    
-    {{N, X, N},
-    {O, O, O},
-    {N, O, N}},
-    
-    {{N, O, N},
-    {O, O, X},
-    {N, O, N}},
-    
-    {{N, O, N},
-    {O, O, O},
-    {N, X, N}},
-    
-    {{N, O, N},
-    {X, O, O},
-    {N, O, N}},
-
-    
-    {{N, X, N},
-    {X, O, X},
-    {N, O, N}},
-    
-    {{N, X, N},
-    {O, O, N},
-    {N, X, N}},
-    
-    {{N, O, N},
-    {X, O, X},
-    {N, N, N}},
-    
-    {{N, X, N},
-    {X, O, O},
-    {N, X, N}},
-
-    
-    {{N, X, N},
-    {X, O, X},
-    {N, X, N}},
-}
-
-local base_shader = data.raw['simple-entity']['h2o-water-shader-1-1-1']
-local filename = '__dihydrogen-monoxide__/graphics/tile/water/water-coastline.png'
-
-local function recursively_generate_coastline_shaders(variants, i)
-    local result = {}
-    if i == #variants then
-        for _, variant in pairs(variants[i]) do
-            result[#result+1] = {variant}
-        end
-        return result
-    end
-    for _, variant in pairs(variants[i]) do
-        local next_variants = recursively_generate_coastline_shaders(variants, i + 1)
-        for _, name_array in pairs(next_variants) do
-            local name_array_copy = table.deepcopy(name_array)
-            table.insert(name_array_copy, 1, variant)
-            result[#result+1] = name_array_copy
-        end
-    end
-    return result
-end
-
-local i = 0
-local y = 0
-for _, grid in pairs(corner_order) do
-    local variants = {}
-    for _, row in pairs(grid) do
-        for _, cell in pairs(row) do
-            local cell_variants = {}
-            if cell == X then
-                cell_variants[#cell_variants+1] = X
-            elseif cell == O then
-                cell_variants[#cell_variants+1] = O
-            else
-                cell_variants[#cell_variants+1] = X
-                cell_variants[#cell_variants+1] = O
-            end
-            variants[#variants+1] = cell_variants
-        end
-    end
-
-    for _, name_array in pairs(recursively_generate_coastline_shaders(variants, 1)) do
-        local name = 'h2o-coastline-shader'
-        for _, n in pairs(name_array) do
-            name = name .. '-' .. n
-        end
-        if data.raw['simple-entity'][name] then goto continue end
-        local water = table.deepcopy(base_shader)
-        water.name = name
-        water.localised_name = name
-        water.animations = {
-            layers = {
-                {
-                    height = 64,
-                    width = 64,
-                    line_length = 2,
-                    variation_count = 2,
-                    filename = filename,
-                    frame_count = 1,
-                    animation_speed = 0.5,
-                    scale = 0.5,
-                    draw_as_glow = false,
-                    shift = {0.5, 0.5},
-                    y = y
-                },
-            }
-        }
-        data:extend{water}
-        i = i + 1
-        ::continue::
-    end
-
-    y = y + 64
-end
-
 local trench_entrance = table.deepcopy(data.raw.tile['out-of-map'])
 trench_entrance.name = 'trench-entrance'
 trench_entrance.layer = 255
 trench_entrance.map_color = {0, 0, 0.1, 1}
-trench_entrance.collision_mask = {'water-tile', 'player-layer', 'object-layer', 'item-layer'}
-data:extend{trench_entrance}
+trench_entrance.collision_mask = {'water-tile', 'player-layer', 'object-layer', 'item-layer', maraxsis_collision_mask}
+data:extend {trench_entrance}
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend {{
+    type = 'double-setting',
+    name = 'h2o-water-opacity',
+    setting_type = 'startup',
+    default_value = 255,
+    minimum_value = 0,
+    maximum_value = 255,
+    order = 'a'
+}}
