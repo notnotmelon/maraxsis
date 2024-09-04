6342a5a885637c15a6132980bd064c062aa3a1b6
notnotmelon <zacharypicco@gmail.com>
2024-09-04 05:49:29 +0200

    feat: Add big cliff explosives

--- prototypes/big-cliff-explosive.lua
+++ prototypes/big-cliff-explosive.lua
@@ -75,7 +75,7 @@ data:extend {{
                     type = 'direct',
                     action_delivery = {
                         type = 'projectile',
-                        projectile = 'cliff-explosives',
+                        projectile = 'h2o-big-cliff-explosives',
                         starting_speed = 0.3
                     }
                 }
@@ -83,3 +83,273 @@ data:extend {{
         }
     },
 }}
+
+data:extend{h2o.merge(data.raw.projectile['cliff-explosives'], {
+    name = 'h2o-big-cliff-explosives',
+    action = {
+        {
+            action_delivery = {
+                target_effects = {
+                    {
+                        decoratives_with_trigger_only = false,
+                        include_decals = true,
+                        include_soft_decoratives = true,
+                        invoke_decorative_trigger = true,
+                        radius = 14,
+                        type = 'destroy-decoratives'
+                    },
+                    {
+                        apply_projection = true,
+                        decorative = 'nuclear-ground-patch',
+                        spawn_max = 40,
+                        spawn_max_radius = 12.5,
+                        spawn_min = 30,
+                        spawn_min_radius = 11.5,
+                        spread_evenly = true,
+                        type = 'create-decorative'
+                    },
+                    {
+                        explosion = 'explosion',
+                        radius = 300,
+                        type = 'destroy-cliffs'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                projectile = 'atomic-bomb-ground-zero-projectile',
+                                starting_speed = 1.48,
+                                starting_speed_deviation = 0.075,
+                                type = 'projectile'
+                            },
+                            radius = 7,
+                            repeat_count = 1000,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    },
+                    {
+                        delay = 0,
+                        duration = 60,
+                        ease_in_duration = 5,
+                        ease_out_duration = 255,
+                        effect = 'screen-burn',
+                        full_strength_max_distance = 400,
+                        max_distance = 1600,
+                        strength = 20,
+                        type = 'camera-effect'
+                    },
+                    {
+                        audible_distance_modifier = 3,
+                        max_distance = 1000,
+                        play_on_target_position = false,
+                        sound = {
+                            aggregation = {
+                                max_count = 1,
+                                remove = true
+                            },
+                            game_controller_vibration_data = {
+                                duration = 800,
+                                low_frequency_vibration_intensity = 1,
+                                play_for = 'everything'
+                            },
+                            switch_vibration_data = {
+                                filename = '__base__/sound/fight/nuclear-explosion.bnvib',
+                                play_for = 'everything'
+                            },
+                            variations = {
+                                {
+                                    filename = '__base__/sound/fight/nuclear-explosion-1.ogg',
+                                    volume = 0.9
+                                },
+                                {
+                                    filename = '__base__/sound/fight/nuclear-explosion-2.ogg',
+                                    volume = 0.9
+                                },
+                                {
+                                    filename = '__base__/sound/fight/nuclear-explosion-3.ogg',
+                                    volume = 0.9
+                                }
+                            }
+                        },
+                        type = 'play-sound'
+                    },
+                    {
+                        audible_distance_modifier = 3,
+                        max_distance = 1000,
+                        play_on_target_position = false,
+                        sound = {
+                            aggregation = {
+                                max_count = 1,
+                                remove = true
+                            },
+                            variations = {
+                                {
+                                    filename = '__base__/sound/fight/nuclear-explosion-aftershock.ogg',
+                                    volume = 0.4
+                                }
+                            }
+                        },
+                        type = 'play-sound'
+                    },
+                    {
+                        repeat_count = 1,
+                        type = 'invoke-tile-trigger'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                projectile = 'atomic-bomb-wave-spawns-cluster-nuke-explosion',
+                                starting_speed = 0.35,
+                                starting_speed_deviation = 0.075,
+                                type = 'projectile'
+                            },
+                            radius = 26,
+                            repeat_count = 1000,
+                            show_in_tooltip = false,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                projectile = 'atomic-bomb-wave-spawns-fire-smoke-explosion',
+                                starting_speed = 1.325,
+                                starting_speed_deviation = 0.075,
+                                type = 'projectile'
+                            },
+                            radius = 4,
+                            repeat_count = 700,
+                            show_in_tooltip = false,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                projectile = 'atomic-bomb-wave-spawns-nuke-shockwave-explosion',
+                                starting_speed = 1.325,
+                                starting_speed_deviation = 0.075,
+                                type = 'projectile'
+                            },
+                            radius = 8,
+                            repeat_count = 1000,
+                            show_in_tooltip = false,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                projectile = 'atomic-bomb-wave-spawns-nuclear-smoke',
+                                starting_speed = 1.325,
+                                starting_speed_deviation = 0.075,
+                                type = 'projectile'
+                            },
+                            radius = 26,
+                            repeat_count = 300,
+                            show_in_tooltip = false,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    },
+                    {
+                        action = {
+                            action_delivery = {
+                                target_effects = {
+                                    {
+                                        entity_name = 'nuclear-smouldering-smoke-source',
+                                        tile_collision_mask = {
+                                            'water-tile'
+                                        },
+                                        type = 'create-entity'
+                                    }
+                                },
+                                type = 'instant'
+                            },
+                            radius = 8,
+                            repeat_count = 10,
+                            show_in_tooltip = false,
+                            target_entities = false,
+                            trigger_from_target = true,
+                            type = 'area'
+                        },
+                        type = 'nested-result'
+                    }
+                },
+                type = 'instant'
+            },
+            type = 'direct'
+        }
+    },
+    animation = {
+        animation_speed = 0.25,
+        draw_as_glow = true,
+        filename = '__base__/graphics/entity/cliff-explosives/cliff-explosives.png',
+        frame_count = 16,
+        height = 30,
+        hr_version = {
+            animation_speed = 0.25,
+            draw_as_glow = true,
+            filename = '__base__/graphics/entity/cliff-explosives/hr-cliff-explosives.png',
+            frame_count = 16,
+            height = 58,
+            line_length = 8,
+            priority = 'high',
+            scale = 0.5,
+            shift = {
+                0.015625,
+                -0.140625
+            },
+            width = 52
+        },
+        line_length = 8,
+        priority = 'high',
+        shift = {
+            0,
+            -0.125
+        },
+        width = 26
+    },
+    shadow = {
+        animation_speed = 0.25,
+        draw_as_shadow = true,
+        filename = '__base__/graphics/entity/cliff-explosives/cliff-explosives-shadow.png',
+        frame_count = 16,
+        height = 22,
+        hr_version = {
+            animation_speed = 0.25,
+            draw_as_shadow = true,
+            filename = '__base__/graphics/entity/cliff-explosives/hr-cliff-explosives-shadow.png',
+            frame_count = 16,
+            height = 42,
+            line_length = 8,
+            priority = 'high',
+            scale = 0.5,
+            shift = {
+                -0.109375,
+                0.125
+            },
+            width = 74
+        },
+        line_length = 8,
+        priority = 'high',
+        shift = {
+            -0.09375,
+            0.125
+        },
+        width = 38
+    },
+})}
