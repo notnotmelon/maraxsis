6b054be9c1ddae1a7f1ebf762737072c04d9e9df
notnotmelon <zacharypicco@gmail.com>
2024-11-29 23:13:43 -0600

    More collision mask fixes

--- prototypes/collision-mask.lua
+++ prototypes/collision-mask.lua
@@ -30,13 +30,22 @@ local prototypes_that_cant_be_placed_on_water = {
     "furnace",
     "generator",
     "market",
+    "storage-tank",
+    "beacon",
     "reactor",
     "roboport",
     "tree",
+    "arithmetic-combinator",
+    "decider-combinator",
+    "constant-combinator",
+    "selector-combinator",
+    "programmable-speaker",
     "power-switch",
     "simple-entity-with-force",
     "simple-entity-with-owner",
-    "solar-panel",
+    "fusion-reactor",
+    "fusion-generator",
+    data.raw.container["sp-spidertron-dock"]
 }
 
 local prototypes_that_cant_be_placed_in_a_dome = {
@@ -48,6 +57,7 @@ local prototypes_that_cant_be_placed_in_a_dome = {
     "cargo-landing-pad",
     "cargo-bay",
     "agricultural-tower",
+    data.raw.radar["maraxsis-sonar"]
 }
 
 local prototypes_that_cant_be_placed_in_a_dome_or_on_water = {
@@ -85,14 +95,16 @@ local prototypes_that_can_be_placed_whereever = {
     data.raw["electric-energy-interface"]["ee-infinity-accumulator-tertiary-buffer"],
     data.raw["spider-leg"]["maraxsis-submarine-leg"],
     data.raw["spider-vehicle"]["maraxsis-diesel-submarine"],
+    data.raw["spider-vehicle"]["spidertron-enhancements-dummy-maraxsis-diesel-submarine"],
     data.raw["spider-vehicle"]["maraxsis-nuclear-submarine"],
+    data.raw["spider-vehicle"]["spidertron-enhancements-dummy-maraxsis-nuclear-submarine"],
+    data.raw.roboport.service_station,
+    data.raw.roboport["maraxsis-pressure-dome"],
     data.raw.tile["maraxsis-pressure-dome-tile"],
 }
 
 for _, anywhere in pairs(prototypes_that_can_be_placed_whereever) do
-    if type(anywhere) == "string" then
-        processed_prototypes[anywhere] = true
-    end
+    processed_prototypes[anywhere.name] = true
 end
 
 local function block_placement_tile(tile, layer)
@@ -107,15 +119,14 @@ local function block_placement_tile(tile, layer)
 
     local place_as_tile = item_to_place.place_as_tile
     place_as_tile.condition = place_as_tile.condition or {}
-    place_as_tile.condition[layer] = true
+    place_as_tile.condition.layers[layer] = true
 end
 
 local function block_placement(prototype, layer)
     if prototype.allow_maraxsis_water_placement then return end -- this check is not used by maraxsis however it may be useful for 3rd party mods doing compatibility
 
-    for _, e in pairs(prototypes_that_can_be_placed_whereever) do
-        if e == prototype then return end
-    end
+    if processed_prototypes[prototype.name] then return end
+    processed_prototypes[prototype.name] = true
 
     if prototype.type and prototype.type == "tile" then
         block_placement_tile(prototype, layer)
@@ -124,7 +135,7 @@ local function block_placement(prototype, layer)
 
     prototype.collision_mask = collision_mask_util.get_mask(prototype)
     if not next(prototype.collision_mask) then goto continue end -- skip if no collision mask to save UPS
-    prototype.collision_mask[layer] = true
+    prototype.collision_mask.layers[layer] = true
     ::continue::
 end
 
@@ -136,7 +147,6 @@ local function add_collision_layer_to_prototypes(prototypes, layer)
             for _, prototype in pairs(data.raw[blacklisted]) do
                 block_placement(prototype, layer)
             end
-            processed_prototypes[blacklisted] = true
         end
     end
 end
@@ -145,11 +155,11 @@ local function remove_collision_layer_to_prototypes(prototypes, layer)
     for _, blacklisted in pairs(prototypes) do
         if type(blacklisted) == "table" then
             blacklisted.collision_mask = collision_mask_util.get_mask(blacklisted)
-            blacklisted.collision_mask[layer] = nil
+            blacklisted.collision_mask.layers[layer] = nil
         else
             for _, prototype in pairs(data.raw[blacklisted]) do
                 prototype.collision_mask = collision_mask_util.get_mask(prototype)
-                prototype.collision_mask[layer] = nil
+                prototype.collision_mask.layers[layer] = nil
             end
         end
     end
@@ -157,11 +167,13 @@ end
 
 local function blacklist_via_surface_condition(entity)
     if processed_prototypes[entity.name] then return end
+    processed_prototypes[entity.name] = true
+
     entity.surface_conditions = entity.surface_conditions or {}
 
     for _, surface_condition in pairs(entity.surface_conditions) do
         if surface_condition.property == "pressure" then
-            assert((surface_condition.max or 0) < 50000)
+            assert((surface_condition.min or 0) < 50000, "An error occurred while blacklisting " .. entity.name .. " from being placed in a dome or on water.")
             surface_condition.max = math.min(50000, surface_condition.max or 50000)
             return
         end
@@ -171,15 +183,14 @@ local function blacklist_via_surface_condition(entity)
         property = "pressure",
         max = 50000
     })
-    processed_prototypes[entity.name] = true
 end
 
 for _, blacklisted in pairs(prototypes_that_cant_be_placed_in_a_dome_or_on_water) do
     if type(blacklisted) == "table" then
-        processed_prototypes[blacklisted.name] = true
+        blacklist_via_surface_condition(blacklisted)
     else
         for _, prototype in pairs(data.raw[blacklisted]) do
-            processed_prototypes[prototype.name] = true
+            blacklist_via_surface_condition(prototype)
         end
     end
 end
@@ -188,7 +199,3 @@ add_collision_layer_to_prototypes(prototypes_that_cant_be_placed_on_water, marax
 remove_collision_layer_to_prototypes(prototypes_that_cant_be_placed_on_water, dome_collision_mask)
 add_collision_layer_to_prototypes(prototypes_that_cant_be_placed_in_a_dome, dome_collision_mask)
 remove_collision_layer_to_prototypes(prototypes_that_cant_be_placed_in_a_dome, maraxsis_collision_mask)
-
-for prototype, processed in pairs(processed_prototypes) do
-    --assert(processed, 'Error in Maraxsis collision mask algorithms! Unrecognized prototype ' .. prototype)
-end
