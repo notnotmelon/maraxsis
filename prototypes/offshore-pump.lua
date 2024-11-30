df14b7601bdfd6f09373e18112e9791dc818f54e
notnotmelon <zacharypicco@gmail.com>
2024-11-29 20:58:29 -0600

    Allow saline water to be obtainable

--- prototypes/offshore-pump.lua
+++ prototypes/offshore-pump.lua
@@ -1,4 +1,3 @@
 local offshore = data.raw["offshore-pump"]["offshore-pump"]
 offshore.tile_buildability_rules[1].required_tiles.layers[maraxsis_collision_mask] = true
 offshore.tile_buildability_rules[2].required_tiles.layers[maraxsis_collision_mask] = true
-offshore.collision_mask = {layers = {}}
