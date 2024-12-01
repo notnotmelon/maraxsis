a9da59ecda9010ab50a92c6ea9d57ca2d0449746
notnotmelon <zacharypicco@gmail.com>
2024-12-01 10:04:41 -0600

    fix: Fixed a crash with 5dims mods.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,3 +2,5 @@ require "prototypes.collision-mask"
 require "prototypes.swimming"
 
 data.raw["technology"]["maraxsis-promethium-productivity"].unit.ingredients = table.deepcopy(data.raw["technology"]["research-productivity"].unit.ingredients)
+
+data.raw.radar["maraxsis-sonar"].next_upgrade = nil -- fix crash with 5dim
