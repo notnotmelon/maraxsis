b87c79ad9d0985793390586d99a20fec26bf1a72
notnotmelon <zacharypicco@gmail.com>
2024-12-03 18:23:47 -0600

    fix: Fixed softlock due to no power in the trench. The trench now has 1% solar energy.

--- prototypes/planet/map-gen.lua
+++ prototypes/planet/map-gen.lua
@@ -272,7 +272,7 @@ data:extend {maraxsis.merge(data.raw.planet.gleba, {
     surface_properties = {
         ["day-night-cycle"] = 5 * minute,
         ["magnetic-field"] = 25,
-        ["solar-power"] = 0,
+        ["solar-power"] = 1,
         pressure = 400000,
         gravity = 20,
     },
