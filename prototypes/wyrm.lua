92f76e85a432ba45fc0a3ed83e07b1ecad839bd1
notnotmelon <zacharypicco@gmail.com>
2024-12-06 15:34:16 -0600

    other: Reduced the cost of the wyrm specimen.

--- prototypes/wyrm.lua
+++ prototypes/wyrm.lua
@@ -59,8 +59,7 @@ data:extend {{
     enabled = false,
     energy_required = 10,
     ingredients = {
-        {type = "item", name = "maraxsis-glass-panes", amount = 10},
-        {type = "item", name = "slowdown-capsule",     amount = 1},
+        {type = "item", name = "maraxsis-glass-panes", amount = 5},
         {type = "item", name = "steel-plate",          amount = 1},
     },
     results = {
