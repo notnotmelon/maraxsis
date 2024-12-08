f4855c7d55726b7e45dfad86d71f4feade1ca6a4
notnotmelon <zacharypicco@gmail.com>
2024-12-07 21:00:15 -0600

    balance: The pressure dome is now crafted with 300 glass instead of 500 glass.

--- prototypes/pressure-dome.lua
+++ prototypes/pressure-dome.lua
@@ -50,7 +50,7 @@ data:extend {{
     enabled = false,
     ingredients = {
         {type = "item", name = "pipe",                 amount = 30},
-        {type = "item", name = "maraxsis-glass-panes", amount = 500},
+        {type = "item", name = "maraxsis-glass-panes", amount = 300},
         {type = "item", name = "tungsten-plate",       amount = 100},
         {type = "item", name = "small-lamp",           amount = 10},
     },
