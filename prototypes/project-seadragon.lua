61d9bf81c6f8fb85bc6835cff5d0a28f0178c507
notnotmelon <zacharypicco@gmail.com>
2024-12-08 21:40:41 -0600

    balance: Increased the crafting time of super sealant substance from 2s -> 5s

--- prototypes/project-seadragon.lua
+++ prototypes/project-seadragon.lua
@@ -65,7 +65,7 @@ data:extend {{
     type = "recipe",
     name = "maraxsis-super-sealant-substance",
     enabled = false,
-    energy_required = 2,
+    energy_required = 5,
     ingredients = {
         {type = "item",  name = "sulfur",    amount = 1},
         {type = "fluid", name = "heavy-oil", amount = 200},
