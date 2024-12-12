b1fa4e3eee563c38865d1f7ccc14a38fd2d18752
notnotmelon <zacharypicco@gmail.com>
2024-12-12 13:06:05 -0600

    balance: Removed surface restrictions on the submarine dock. Can now be placed anywhere!

--- prototypes/spidertron-patrols.lua
+++ prototypes/spidertron-patrols.lua
@@ -19,15 +19,3 @@ data.raw.recipe["sp-spidertron-dock"].ingredients = {
     {type = "item", name = "tungsten-plate", amount = 16},
     {type = "item", name = "bulk-inserter",  amount = 4},
 }
-
-local function add_surface_conditions_to_submarine_dock(dock_name)
-    local dock = data.raw.container[dock_name]
-    if not dock then return end
-    dock.surface_conditions = maraxsis.surface_conditions()
-end
-
-for i = 1, 200 do
-    add_surface_conditions_to_submarine_dock("sp-spidertron-dock-" .. i)
-end
-add_surface_conditions_to_submarine_dock("sp-spidertron-dock")
-add_surface_conditions_to_submarine_dock("sp-spidertron-dock-closing")
