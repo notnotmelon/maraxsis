32b9f750feea429293ca909285bb7e5f542658ff
notnotmelon <zacharypicco@gmail.com>
2024-12-21 23:12:04 -0600

    balance: The conduit technology now only requires fulgora and maraxsis science packs.

--- prototypes/conduit.lua
+++ prototypes/conduit.lua
@@ -42,15 +42,7 @@ data:extend {{
     unit = {
         count = 5000,
         ingredients = {
-            {"automation-science-pack",      1},
-            {"logistic-science-pack",        1},
-            {"chemical-science-pack",        1},
-            {"space-science-pack",           1},
-            {"production-science-pack",      1},
-            {"utility-science-pack",         1},
-            {"metallurgic-science-pack",     1},
             {"electromagnetic-science-pack", 1},
-            {"agricultural-science-pack",    1},
             {"hydraulic-science-pack",       1},
         },
         time = 60,
