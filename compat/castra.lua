10b517eb25cfbfeb09b0281f77af147b3e9ce51a
Zachary Picco <zacharypicco@gmail.com>
2025-02-25 10:52:32 -0600

    compat: Fixed a crash with the castra mod. Resolves https://github.com/notnotmelon/maraxsis/issues/228

--- compat/castra.lua
+++ compat/castra.lua
@@ -1,3 +1,4 @@
-data.raw.technology["cargo-landing-pad-capacity"].unit.ingredients = table.dedupe(
-    data.raw.technology["cargo-landing-pad-capacity"].unit.ingredients
-)
+local landing_pad_capacity = data.raw.technology["cargo-landing-pad-capacity"]
+if not landing_pad_capacity then return end
+
+landing_pad_capacity.unit.ingredients = table.dedupe(landing_pad_capacity.unit.ingredients)
