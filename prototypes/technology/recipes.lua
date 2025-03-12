ef36dc8d7c944aee30a8ded19d9f7a0080952ec0
Zachary Picco <zacharypicco@gmail.com>
2025-03-12 10:49:32 -0500

    balance: Reverted "Removed petroleum gas cracking into heavy oil."

--- prototypes/technology/recipes.lua
+++ prototypes/technology/recipes.lua
@@ -20,3 +20,26 @@ data:extend {{
     icon_size = 64,
     allow_productivity = true,
 }}
+
+data:extend {{
+    type = "recipe",
+    name = "maraxsis-petroleum-gas-cracking",
+    ingredients = {
+        {type = "fluid", name = "petroleum-gas", amount = 40},
+        {type = "fluid", name = "water",         amount = 30},
+    },
+    results = {
+        {type = "fluid", name = "heavy-oil", amount = 10},
+    },
+    allow_productivity = true,
+    category = "maraxsis-hydro-plant",
+    energy_required = 2,
+    subgroup = "fluid-recipes",
+    order = "b[fluid-chemistry]-c[petroleum-gas-cracking]",
+    enabled = false,
+    icon = "__maraxsis__/graphics/icons/petroleum-gas-cracking.png",
+    icon_size = 64,
+    auto_recycle = false,
+    allow_decomposition = false,
+    maximum_productivity = 1.5,
+}}
