2f2270f15bd6ac944cd0e9deb48e0ff54011e141
Zachary Picco <zacharypicco@gmail.com>
2025-04-24 19:52:13 -0500

    fix: Fix dumb

--- scripts/constants.lua
+++ scripts/constants.lua
@@ -29,7 +29,7 @@ local MARAXSIS_SAND_EXTRACTORS = {
 
 local SUBMARINE_FUEL_SOURCES = {
     ["maraxsis-diesel-submarine"] = {"maraxsis-diesel", "rocket-fuel"},
-    ["maraxsis-nuclear-submarine"] = mods.pystellarexpedition and {"nuclear", "nuclear-fuel"} or {"nuclear", "nuclear-fuel", "maraxsis-salt-reactor"},
+    ["maraxsis-nuclear-submarine"] = {"nuclear", "nuclear-fuel", "maraxsis-salt-reactor"},
 }
 
 local DOME_DISABLEABLE_TYPES = {
