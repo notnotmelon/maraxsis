4e321265f72df45961867863ae933683bcffd691
Zachary Picco <zacharypicco@gmail.com>
2025-08-05 09:08:40 -0500

    locale: Removed "can void any fluid" from the hydro plant description while either cerys or k2 are installed.

--- prototypes/fluid-void.lua
+++ prototypes/fluid-void.lua
@@ -1,6 +1,12 @@
-if mods["Krastorio2-spaced-out"] or mods["Krastorio2"] then return end
+if mods["Krastorio2-spaced-out"] or mods["Krastorio2"] then
+    data.raw["assembling-machine"]["maraxsis-hydro-plant"].localised_description = {"entity-description.maraxsis-hydro-plant-cerys"}
+    data.raw["assembling-machine"]["maraxsis-hydro-plant-extra-module-slots"].localised_description = {"entity-description.maraxsis-hydro-plant-cerys"}
+    return
+end
 
 if mods["Flare Stack"] then
+    data.raw["assembling-machine"]["maraxsis-hydro-plant"].localised_description = {"entity-description.maraxsis-hydro-plant-cerys"}
+    data.raw["assembling-machine"]["maraxsis-hydro-plant-extra-module-slots"].localised_description = {"entity-description.maraxsis-hydro-plant-cerys"}
     data.raw.furnace["flare-stack"].module_slots = 1
     data.raw.furnace["flare-stack"].allowed_effects = {"consumption", "speed", "pollution", "quality"}
     return
