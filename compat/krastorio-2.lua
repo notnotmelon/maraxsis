67341c7df8089a7042b137eba675d96707c87b5e
Polka37 <209836559+Polka37@users.noreply.github.com>
2025-09-30 21:32:12 +0200

    compat: Delete deprecated K2SO compatibility code (#334)

--- compat/krastorio-2.lua
+++ compat/krastorio-2.lua
@@ -151,6 +151,3 @@ data.raw.recipe["maraxsis-microplastics"].ingredients = {
     {type = "item", name = "maraxsis-tropical-fish",   amount = 1},
     {type = "item", name = "shotgun-shell", amount = 1},
 }
-
-data.raw.fluid["kr-hydrogen"].auto_barrel = false
-data.raw.fluid["kr-oxygen"].auto_barrel = false
