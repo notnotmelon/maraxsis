0affe0717b67ef1bdcf62a89d2692ff00425cac4
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-05 12:49:34 -0700

    compat: Replaced all rocket silo scripts and Maraxsis' exclusive rocket silo entity with PlanetsLib rocket part recipe assignment, whose code is itself forked from an older version of Maraxsis' rocket silo script.

---
+++ migrations/PlanetsLib-rocket-silo.lua
@@ -0,0 +1,10 @@
+local rocket_silo_lib = require("__PlanetsLib__.scripts.rocket-parts")
+if game.planets["maraxsis"] then
+    local maraxsis_rocket_silos = game.planets["maraxsis"].surface.find_entities_filtered{{filter_type = "name",name="rocket-silo"}}
+    for _,silo in pairs(maraxsis_rocket_silos) do
+        local fake_event = {
+            entity = silo
+        }
+        rocket_silo_lib.on_built_rocket_silo(fake_event) --Runs PlanetsLib's on_built_rocket_silo code, which locks the rocket silo if configured to. See PlanetsLib.assign_rocket_part_recipe.
+    end
+end
