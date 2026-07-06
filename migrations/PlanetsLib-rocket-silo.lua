e2b6ec6db18c1ba0274c90edd26e82d1de121bc7
ari <aripicco@proton.me>
2026-07-05 20:32:51 -0700

    fix: Fixed broken migration involving rocket silos

--- migrations/PlanetsLib-rocket-silo.lua
+++ migrations/PlanetsLib-rocket-silo.lua
@@ -1,10 +1,15 @@
 local rocket_silo_lib = require("__PlanetsLib__.scripts.rocket-parts")
-if game.planets["maraxsis"] and game.planets["maraxsis"].surface then
-    local maraxsis_rocket_silos = game.planets["maraxsis"].surface.find_entities_filtered{{filter_type = "name",name="rocket-silo"}}
-    for _,silo in pairs(maraxsis_rocket_silos) do
-        local fake_event = {
-            entity = silo
-        }
-        rocket_silo_lib.on_built_rocket_silo(fake_event) --Runs PlanetsLib's on_built_rocket_silo code, which locks the rocket silo if configured to. See PlanetsLib.assign_rocket_part_recipe.
-    end
+
+if not game.planets["maraxsis"] or not game.planets["maraxsis"].surface then
+    return
+end
+
+local maraxsis_rocket_silos = game.planets["maraxsis"].surface.find_entities_filtered{type = "rocket-silo"}
+for _, silo in pairs(maraxsis_rocket_silos) do
+    local fake_event = {
+        entity = silo
+    }
+
+    -- Runs PlanetsLib's on_built_rocket_silo code, which locks the rocket silo if configured to. See PlanetsLib.assign_rocket_part_recipe.
+    rocket_silo_lib.on_built_rocket_silo(fake_event)
 end
