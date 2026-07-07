42aa3e8522cc31ea5e2a7d883a1808a3bdcf6513
Nicholas Gower <nicholasgower01@gmail.com>
2026-07-06 18:26:50 -0700

    Moved extra-module-slots to end of file

--- prototypes/entity/hydro-plant.lua
+++ prototypes/entity/hydro-plant.lua
@@ -173,17 +173,8 @@ data:extend { {
     collision_mask = { layers = { item = true, object = true, player = true, water_tile = true } },
 } }
 
-local extra_module_slots = table.deepcopy(data.raw["assembling-machine"]["maraxsis-hydro-plant"])
-extra_module_slots.name = "maraxsis-hydro-plant-extra-module-slots"
-extra_module_slots.module_slots = extra_module_slots.module_slots + 4
-extra_module_slots.icons_positioning = { {
-    inventory_index = defines.inventory.crafter_modules, shift = { 0, 0.9 }, max_icons_per_row = 4
-} }
-extra_module_slots.hidden_in_factoriopedia = true
-extra_module_slots.factoriopedia_alternative = "maraxsis-hydro-plant"
-extra_module_slots.placeable_by = { { item = "maraxsis-hydro-plant", count = 1 } }
-extra_module_slots.flags = { "placeable-player", "player-creation", "not-in-made-in" }
-data:extend { extra_module_slots }
+
+
 
 data:extend { {
     type = "item",
@@ -247,3 +238,16 @@ table.insert(data.raw.technology["holmium-processing"].effects, {
     type = "unlock-recipe",
     recipe = "maraxsis-holmium-recrystalization"
 })
+
+local extra_module_slots = table.deepcopy(data.raw["assembling-machine"]["maraxsis-hydro-plant"])
+extra_module_slots.name = "maraxsis-hydro-plant-extra-module-slots"
+extra_module_slots.module_slots = extra_module_slots.module_slots + 4
+extra_module_slots.icons_positioning = { {
+    inventory_index = defines.inventory.crafter_modules, shift = { 0, 0.9 }, max_icons_per_row = 4
+} }
+extra_module_slots.hidden_in_factoriopedia = true
+extra_module_slots.factoriopedia_alternative = "maraxsis-hydro-plant"
+extra_module_slots.placeable_by = { { item = "maraxsis-hydro-plant", count = 1 } }
+extra_module_slots.flags = { "placeable-player", "player-creation", "not-in-made-in" }
+
+data:extend { extra_module_slots }
